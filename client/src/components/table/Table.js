372ba72c990c9de3c8cee71e55065324dff9f552
IvanZaborskih <ivan.31.03@mail.ru>
2022-05-06 20:04:41 +0300

    Return client

---
+++ client/src/components/table/Table.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import './table.scss';
+import axios from '../../axios/api';
+
+const Table = () => {
+	const [persons, setPersons] = useState([]);
+
+	useEffect(() => {
+		const getPersons = async () => {
+			try {
+				const response = await axios.get('/user');
+				setPersons(response.data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		getPersons();
+	}, []);
+
+	return (
+		<div>{
+			persons.length > 0 ? <table>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Email</th>
+						<th>Phone</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					{persons.map(item => (
+						<tr key={item.id}>
+							<td> {item.name} </td>
+							<td> {item.email} </td>
+							<td> {item.phone} </td>
+							<td> <a>E</a> <a>D</a> </td>
+						</tr>
+					))}
+				</tbody>
+			</table> : <table>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Email</th>
+						<th>Phone</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>...</td>
+						<td>...</td>
+						<td>...</td>
+						<td>...</td>
+					</tr>
+				</tbody>
+			</table>
+		}</div>
+	)
+}
+
+export default Table;
