dee531ce0210f020dcd655aaae3afa566cc73dc5
IvanZaborskih <ivan.31.03@mail.ru>
2022-05-06 22:09:50 +0300

    Add edit, delete and post

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,46 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Table from './table/Table';
+import axios from '../axios/api';
+import Modal from "./modals/modal";
 
-class App extends Component {
-	render() {
-		return (
-			<div>
-				<Table></Table>
-			</div>
-		)
-	}
+function App() {
+	const [persons, setPersons] = useState([]);
+	const [open, setOpen] = useState(false);
+	const [data, setData] = useState({});
+
+	useEffect(() => {
+		const getPersons = async () => {
+			try {
+				const response = await axios.get('/user');
+				setPersons(response.data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		getPersons();
+	}, []);
+
+	return (
+		<div>
+			<Table
+				persons={persons}
+				setPersons={setPersons}
+				open={open}
+				setOpen={setOpen}
+				data={data}
+				setData={setData}
+			></Table>
+			<Modal
+				persons={persons}
+				setPersons={setPersons}
+				open={open}
+				setOpen={setOpen}
+				data={data}
+				setData={setData}
+			></Modal>
+		</div>
+	)
 }
 
 export default App;
